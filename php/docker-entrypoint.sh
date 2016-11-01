b5aa0b0941774dcc5e13a10d8894e5e79708a359
Fabio Montefuscolo <fabio.montefuscolo@gmail.com>
2016-11-01 11:20:33 -0200

    add xdebug

--- php/docker-entrypoint.sh
+++ php/docker-entrypoint.sh
@@ -1,2 +1,25 @@
 #!/bin/bash
+if [ -n "$XDEBUG" ];
+then
+    inifile="/usr/local/etc/php/conf.d/pecl-xdebug.ini"
+    extfile="$(find /usr/local/lib/php/extensions/ -name xdebug.so)";
+    remote_port="${XDEBUG_IDEKEY:-9000}";
+    idekey="${XDEBUG_IDEKEY:-xdbg}";
+
+    if [ -f "$extfile" ] && [ ! -f "$inifile" ];
+    then
+        {
+            echo "[Xdebug]";
+            echo "zend_extension=${extfile}";
+            echo "xdebug.idekey=${idekey}";
+            echo "xdebug.remote_enable=1";
+            echo "xdebug.remote_connect_back=1";
+            echo "xdebug.remote_autostart=1";
+            echo "xdebug.remote_port=${remote_port}";
+        } > $inifile;
+    fi
+
+    unset extfile remote_port idekey;
+fi
+
 exec "$@"
