c8fb4dbb17eea168e7f76ffbab931ae593c57ab6
Guill Lo <guill.bout@gmail.com>
2019-08-03 15:55:24 +0800

    Add different xdebug config for macos/windows and linux
    (cherry picked from commit 4918913d0bc143530d8bc4a65f7e5dbcae39db0c)

--- root/entrypoint.sh
+++ root/entrypoint.sh
@@ -8,6 +8,7 @@ then
     inifile="/usr/local/etc/php/conf.d/pecl-xdebug.ini"
     extfile="$(find /usr/local/lib/php/extensions/ -name xdebug.so)";
     remote_port="${XDEBUG_REMOTE_PORT:-9000}";
+    remote_os="${XDEBUG_REMOTE_OS:-linux}";
     idekey="${XDEBUG_IDEKEY:-xdbg}";
 
     if [ -f "$extfile" ] && [ ! -f "$inifile" ];
@@ -17,10 +18,22 @@ then
             echo "zend_extension=${extfile}";
             echo "xdebug.idekey=${idekey}";
             echo "xdebug.remote_enable=1";
-            echo "xdebug.remote_connect_back=1";
             echo "xdebug.remote_autostart=1";
             echo "xdebug.remote_port=${remote_port}";
         } > $inifile;
+
+        if [ $remote_os = "macos" ] || [ $remote_os = "windows"];
+        then
+            {
+                echo "xdebug.remote_connect_back=0";
+                echo "xdebug.remote_host=host.docker.internal";
+            } >> $inifile;
+        else
+            {
+                echo "xdebug.remote_connect_back=1";
+            } >> $inifile;
+        fi
+
     fi
 
     unset extfile remote_port idekey;
